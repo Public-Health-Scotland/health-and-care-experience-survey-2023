f677a5f19e3614d42a609b221552bb8c64e1d30a
catrionah <catriona.haddow@phs.scot>
2023-09-06 15:48:57 +0100

    added set up codes plus a first draft of an analytical code

---
+++ 00.set_up_file_paths.R
@@ -0,0 +1,18 @@
+# Name of file: 00.setup_file_paths.R
+# 
+# Original author(s): Catriona Haddow
+#   
+# Written/run on: Posit Workbench - RStudio R 4.1.2
+# 
+# Description of content: Set up file paths to be used throughout project
+# 
+# Approximate run time: <1 min
+# 
+# Approximate memory usage: 245mib
+
+lookup_path   <- if_else(substr(getwd(),2,5) == "conf","/conf/bss/pat-exp-surveys/health-and-care/202324/lookups/","someothernetwork/lookups/")
+data_path     <- if_else(substr(getwd(),2,5) == "conf","/conf/bss/pat-exp-surveys/health-and-care/202324/data/","someothernetwork/data/")
+output_path   <- if_else(substr(getwd(),2,5) == "conf","/conf/bss/pat-exp-surveys/health-and-care/202324/output/","someothernetwork/output/")
+
+
+
