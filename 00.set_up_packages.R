509f48cd80a0d02cafbbab9ee1ec8ddce2178559
catrionah <catriona.haddow@phs.scot>
2024-02-20 16:41:58 +0000

    Removed PHS packages from set up as SG cannot access Corrected file paths from output to analysis_output

--- 00.set_up_packages.R
+++ 00.set_up_packages.R
@@ -3,6 +3,5 @@ library(readxl) #version 1.3.1
 library(openxlsx) #version 4.2.5.2 
 library(lubridate) #version 1.9.1 
 library(srvyr) #version 1.2.7
-library(phsmethods) #version 0.2.2.0
 library(janitor) #version 2.2.0
 
